e7e0c0f04b3973e0daa963c3688f2ce183f941c0
John Favorite <john.favorite@gmail.com>
2018-12-02 00:01:08 -0500

    boilerplate working

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,32 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_chrony_is_installed(host):
+    package = host.package('chrony')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert package.is_installed
+
+
+def test_chronyd_running_and_enabled(host):
+    chronyd = host.service('chronyd')
+
+    assert chronyd.is_running
+    assert chronyd.is_enabled
+
+
+def test_centos_release_openstack_rocky_is_installed(host):
+    package = host.package('centos-release-openstack-rocky')
+
+    assert package.is_installed
+
+
+def test_python_openstackclient_is_installed(host):
+    package = host.package('python2-openstackclient')
+
+    assert package.is_installed
+
+
+def test_openstack_selinux_is_installed(host):
+    package = host.package('openstack-selinux')
+
+    assert package.is_installed
